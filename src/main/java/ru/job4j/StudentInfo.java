2753bf8b1fcc1707513cbec958fce956dcc4d6b9
Bagrat <bagiration@yandex.ru>
2022-09-21 21:43:35 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Bagrat Paronian");
+        System.out.printf("02.01.1984");
+    }
+}
