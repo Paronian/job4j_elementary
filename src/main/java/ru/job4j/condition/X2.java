2bcf5ad96b7bf764a1387189cd0fb0077f779ab3
Bagrat <bagiration@yandex.ru>
2022-10-02 09:53:25 +0300

    Убрал комментарии

--- src/main/java/ru/job4j/condition/X2.java
+++ src/main/java/ru/job4j/condition/X2.java
@@ -7,11 +7,11 @@ public class X2 {
     }
 
     public static void main(String[] args) {
-        int a = 10;
-        int b = 0;
-        int c = 0;
-        int x = 2;
-        int rsl = X2.calc(a, b, c, x);
-        System.out.println(rsl);
+         int a = 10;
+         int b = 0;
+         int c = 0;
+         int x = 2;
+         int rsl = X2.calc(a, b, c, x);
+         System.out.println(rsl);
     }
 }
