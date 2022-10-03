f6b25def7f92fccc795fc732175f831d1b4d4e96
Bagrat <bagiration@yandex.ru>
2022-10-03 23:50:13 +0300

    Сделал понятным и более читабельным код.

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -2,11 +2,13 @@ package ru.job4j.condition;
 
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
-        int rsl = 0;
-        if (isValid(x1) && isValid(x2) && isValid(y1) && isValid(y2) && (x1 != x2) && Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
+        if (isValid(x1) && isValid(x2)
+                && isValid(y1) && isValid(y2)
+                && (x1 != x2) && Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
             return Math.abs(y2 - y1);
+        } else {
+            return 0;
         }
-        return rsl;
     }
 
     private static boolean isValid(int coordinate) {
@@ -16,5 +18,4 @@ public class ChessBoard {
     public static void main(String[] args) {
         System.out.println(way(6, 7, 1, 2));
     }
-
 }
