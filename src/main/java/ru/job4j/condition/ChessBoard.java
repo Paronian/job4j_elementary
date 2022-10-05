44afe295dae2f0a427f0df474eca2679fa550456
Bagrat <bagiration@yandex.ru>
2022-10-06 00:30:43 +0300

    4.3.6. Math.abs [#373616 #348077] (Переделал код на более лаконичный.)

--- src/main/java/ru/job4j/condition/ChessBoard.java
+++ src/main/java/ru/job4j/condition/ChessBoard.java
@@ -2,7 +2,7 @@ package ru.job4j.condition;
 
 public class ChessBoard {
     public static int way(int x1, int y1, int x2, int y2) {
-//
+
         int rsl = (isValid(x1) && isValid(x2)
                 && isValid(y1) && isValid(y2)
                 && (x1 != x2)
