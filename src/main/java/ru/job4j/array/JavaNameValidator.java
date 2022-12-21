2ef333fbf03bb7d9e63d18260f42168f19f75343
Bagrat <bagiration@yandex.ru>
2022-12-21 23:57:58 +0300

    6.9. String - это массив [#504884 #351983]

--- src/main/java/ru/job4j/array/JavaNameValidator.java
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -4,43 +4,22 @@ import static java.lang.Character.*;
 
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
-        boolean result = true;
-        char[] codeArray = name.toCharArray();
-        if (name.isEmpty()) {
-            result = false;
-        } else {
-            for (int i = 0; i < codeArray.length; i++) {
+        boolean result = !name.isEmpty() && isLowerCase(name.codePointAt(0))
+                                         && !isDigit(name.codePointAt(0));
+        if (result) {
+            for (int i = 1; i < name.length(); i++) {
                 int code = name.codePointAt(i);
-                if (i == 0 && !isLowerLatinLetter(code)) {
+                if (!isSpecialSymbol(code) && !isUpperCase(code) && !isLowerCase(code) && !isDigit(code)) {
                     result = false;
                     break;
-                } else {
-                    if (isSpecialSymbol(code) || isUpperLatinLetter(code) || isLowerLatinLetter(code) || isNumber(code)) {
-                        result = true;
-                    } else {
-                        result = false;
-                        break;
-                    }
                 }
             }
         }
         return result;
     }
 
-    public static boolean isSpecialSymbol(int code) {
+    private static boolean isSpecialSymbol(int code) {
         return code == 36 || code == 95;
-    }
-
-    public static boolean isUpperLatinLetter(int code) {
-        return isUpperCase(code);
-    }
-
-    public static boolean isLowerLatinLetter(int code) {
-        return isLowerCase(code);
-    }
-
-    public static boolean isNumber(int code) {
-        return isDigit(code);
     }
 }
 
